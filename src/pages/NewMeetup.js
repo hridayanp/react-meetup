630e36cf0d191857f6193ea44ff77ac1c8ccc727
Hridayan Phukan <hridayanphukan123@gmail.com>
2021-05-28 23:24:24 +0530

    Added Meetup Form (skeleton only).

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetup(){
     return(
-        <div>New Meetups Page</div>
+        <section>
+            <h1>Add New Meetups</h1>
+            <NewMeetupForm />
+        </section>
     );
 }
 export default NewMeetup;
